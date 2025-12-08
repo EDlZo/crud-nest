31c3b8ab4961889b2a79cfc70d2bebb1f00d9cb8
Umaporn Poodproh <umapornpoodproh@Jjooyy-MacBook-Air.local>
2025-12-08 09:08:11 +0700

    Add class-validator decorators to CreateCrudDto for proper validation

--- src/cruds/dto/create-crud.dto.ts
+++ src/cruds/dto/create-crud.dto.ts
@@ -1,8 +1,22 @@
+import { IsString, IsOptional, IsEmail } from 'class-validator';
+
 export class CreateCrudDto {
+  @IsString()
   firstName: string;
+
+  @IsString()
   lastName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
   phone: string;
+
+  @IsString()
   address: string;
+
+  @IsOptional()
+  @IsString()
   photo?: string;
 }
